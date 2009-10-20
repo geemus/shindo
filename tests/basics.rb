c9c6fae5cd27f08ba96d0431b5d438059dcfdcf4
Wesley Beary <wbeary@engineyard.com>
2009-10-19 21:48:01 -0700

    more do/end for multi-line blocks

--- tests/basics.rb
+++ tests/basics.rb
@@ -2,39 +2,39 @@ require File.join(File.dirname(__FILE__), 'tests_helper')
 
 success = Tempfile.new('success')
 success << <<-TESTS
-Shindo.tests {
+Shindo.tests do
   test('success') { true }
-}
+end
 TESTS
 success.close
 
 failure = Tempfile.new('failure')
 failure << <<-TESTS
-Shindo.tests {
+Shindo.tests do
   test('failure') { false }
-}
+end
 TESTS
 failure.close
 
 pending = Tempfile.new('pending')
 pending << <<-TESTS
-Shindo.tests {
+Shindo.tests do
   test('pending')
-}
+end
 TESTS
 pending.close
 
-Shindo.tests('basics') {
-  tests('failure') {
+Shindo.tests('basics') do
+  tests('failure') do
     test('output') { `#{BIN} #{failure.path}`.include?('- failure') }
     test('status') { $?.exitstatus == 1 }
-  }
-  tests('pending') {
+  end
+  tests('pending') do
     test('output') { `#{BIN} #{pending.path}`.include?('* pending') }
     test('status') { $?.exitstatus == 0 }
-  }
-  tests('success') {
+  end
+  tests('success') do
     test('output') { `#{BIN} #{success.path}`.include?('+ success') }
     test('status') { $?.exitstatus == 0 }
-  }
-}
+  end
+end
