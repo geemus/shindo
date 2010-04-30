f45f928b363708968801ba79cda38b5e2c8b6582
geemus <geemus@gmail.com>
2010-04-29 22:39:09 -0700

    just shell out once per set of assertions

--- tests/basic_tests.rb
+++ tests/basic_tests.rb
@@ -1,29 +1,21 @@
 Shindo.tests('basics') do
   tests('exception') do
-    before do
-      @output = bin(path('exception'))
-    end
+    @output = bin(path('exception'))
     test('output') { @output.include?('- exception') }
     test('status') { $?.exitstatus == 1 }
   end
   tests('failure') do
-    before do
-      @output = bin(path('failure'))
-    end
+    @output = bin(path('failure'))
     test('output') { @output.include?('- failure') }
     test('status') { $?.exitstatus == 1 }
   end
   tests('pending') do
-    before do
-      @output = bin(path('pending'))
-    end
+    @output = bin(path('pending'))
     test('output') { @output.include?('* pending') }
     test('status') { $?.exitstatus == 0 }
   end
   tests('success') do
-    before do
-      @output = bin(path('success'))
-    end
+    @output = bin(path('success'))
     test('output') { @output.include?('+ success') }
     test('status') { $?.exitstatus == 0 }
   end
