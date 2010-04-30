0dd2733c3649fe78f4822d2c1cac4081fa7b066b
geemus <geemus@gmail.com>
2010-04-29 23:29:01 -0700

    doc fix about _helper.rb files and whitespace fix for tests

--- tests/basic_tests.rb
+++ tests/basic_tests.rb
@@ -1,22 +1,27 @@
 Shindo.tests('basics') do
+
   tests('exception') do
     @output = bin(path('exception'))
     test('output') { @output.include?('- exception') }
     test('status') { $?.exitstatus == 1 }
   end
+
   tests('failure') do
     @output = bin(path('failure'))
     test('output') { @output.include?('- failure') }
     test('status') { $?.exitstatus == 1 }
   end
+
   tests('pending') do
     @output = bin(path('pending'))
     test('output') { @output.include?('* pending') }
     test('status') { $?.exitstatus == 0 }
   end
+
   tests('success') do
     @output = bin(path('success'))
     test('output') { @output.include?('+ success') }
     test('status') { $?.exitstatus == 0 }
   end
+
 end
