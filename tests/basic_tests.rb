f44c617c37a2e889b4f678ab6dc5a73e988056c7
geemus <geemus@gmail.com>
2010-05-14 18:49:18 -0700

    nicer whitespace/formatting in tests

--- tests/basic_tests.rb
+++ tests/basic_tests.rb
@@ -1,30 +1,30 @@
 Shindo.tests('basics') do
 
-  returns(false) { false }
+  returns(false)        { false }
   raises(StandardError) { raise StandardError.new }
 
   tests('exception') do
     @output = bin(path('exception'))
     includes('- exception') { @output }
-    returns(1)    { $?.exitstatus }
+    returns(1)              { $?.exitstatus }
   end
 
   tests('failure') do
     @output = bin(path('failure'))
     includes('- failure') { @output }
-    returns(1)  { $?.exitstatus }
+    returns(1)            { $?.exitstatus }
   end
 
   tests('pending') do
     @output = bin(path('pending'))
     includes('# pending') { @output }
-    returns(0)  { $?.exitstatus }
+    returns(0)            { $?.exitstatus }
   end
 
   tests('success') do
     @output = bin(path('success'))
     includes('+ success') { @output }
-    returns(0) { $?.exitstatus }
+    returns(0)            { $?.exitstatus }
   end
 
 end
--- tests/tag_tests.rb
+++ tests/tag_tests.rb
@@ -4,14 +4,14 @@ Shindo.tests('tags') do
     @output = bin("#{path('negative')} -negative")
     includes('+ is tested')         { @output }
     includes('skipped (negative)')  { @output }
-    returns(0)            { $?.exitstatus }
+    returns(0)                      { $?.exitstatus }
   end
 
   tests('positive') do
     @output = bin("#{path('positive')} +positive")
     includes('+ is tested') { @output }
     includes('skipped')     { @output }
-    returns(0)    { $?.exitstatus }
+    returns(0)              { $?.exitstatus }
   end
 
 end
