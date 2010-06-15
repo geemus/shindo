666e543c09174c813a5ce25296bacf57d874a710
geemus <geemus@gmail.com>
2010-06-14 22:12:02 -0700

    move current reporting/running function into verbose

--- lib/shindo.rb
+++ lib/shindo.rb
@@ -151,34 +151,6 @@ module Shindo
       success
     end
 
-    def display_description(description)
-      Thread.current[:formatador].display_line(description)
-    end
-
-    def display_error(error)
-      Thread.current[:formatador].display_line("[red]#{error.message} (#{error.class})[/]")
-      unless error.backtrace.empty?
-        Thread.current[:formatador].indent do
-          Thread.current[:formatador].display_lines(error.backtrace.map {|line| "[red]#{line}[/]"})
-        end
-      end
-    end
-
-    def display_failure(description)
-      Thread.current[:totals][:failed] += 1
-      Thread.current[:formatador].display_line("[red]- #{description}[/]")
-    end
-
-    def display_pending(description)
-      Thread.current[:totals][:pending] += 1
-      Thread.current[:formatador].display_line("[yellow]# #{description}[/]")
-    end
-
-    def display_success(description)
-      Thread.current[:totals][:succeeded] += 1
-      Thread.current[:formatador].display_line("[green]+ #{description}[/]")
-    end
-
     def prompt(description, &block)
       return if Thread.main[:exit] || Thread.current[:reload]
       Thread.current[:formatador].display("Action? [c,e,i,q,r,t,?]? ")
@@ -248,16 +220,6 @@ module Shindo
         Thread.current[:formatador].display_line("[red]- #{description}[/]")
         prompt(description, &block)
       end
-    end
-
-    def raises?(expectation, &block)
-      @gestalt = Gestalt.new({'formatador' => Thread.current[:formatador]})
-      [value = @gestalt.run(&block), value.is_a?(expectation)]
-    end
-
-    def returns?(expectation, &block)
-      @gestalt = Gestalt.new({'formatador' => Thread.current[:formatador]})
-      [value = @gestalt.run(&block), value == expectation]
     end
 
   end
---
+++ lib/shindo/verbose.rb
@@ -0,0 +1,43 @@
+module Shindo
+  class Tests
+
+    def display_description(description)
+      Thread.current[:formatador].display_line(description)
+    end
+
+    def display_error(error)
+      Thread.current[:formatador].display_line("[red]#{error.message} (#{error.class})[/]")
+      unless error.backtrace.empty?
+        Thread.current[:formatador].indent do
+          Thread.current[:formatador].display_lines(error.backtrace.map {|line| "[red]#{line}[/]"})
+        end
+      end
+    end
+
+    def display_failure(description)
+      Thread.current[:totals][:failed] += 1
+      Thread.current[:formatador].display_line("[red]- #{description}[/]")
+    end
+
+    def display_pending(description)
+      Thread.current[:totals][:pending] += 1
+      Thread.current[:formatador].display_line("[yellow]# #{description}[/]")
+    end
+
+    def display_success(description)
+      Thread.current[:totals][:succeeded] += 1
+      Thread.current[:formatador].display_line("[green]+ #{description}[/]")
+    end
+
+    def raises?(expectation, &block)
+      @gestalt = Gestalt.new({'formatador' => Thread.current[:formatador]})
+      [value = @gestalt.run(&block), value.is_a?(expectation)]
+    end
+
+    def returns?(expectation, &block)
+      @gestalt = Gestalt.new({'formatador' => Thread.current[:formatador]})
+      [value = @gestalt.run(&block), value == expectation]
+    end
+
+  end
+end
