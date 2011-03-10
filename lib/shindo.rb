9ee72d0692360ad317d8e460f011916a55cec3ff
geemus <geemus@gmail.com>
2011-03-10 15:41:35 -0800

    Release 0.3.0

--- lib/shindo.rb
+++ lib/shindo.rb
@@ -6,7 +6,7 @@ module Shindo
   class Pending < StandardError; end
 
   unless const_defined?(:VERSION)
-    VERSION = '0.2.2'
+    VERSION = '0.3.0'
   end
 
   def self.tests(description = nil, tags = [], &block)
